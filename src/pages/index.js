f90c9f87e9c0794d54c6c33428c35346117e2daa
iogrids <iogrids1@gmail.com>
2021-09-14 23:18:45 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Layout from './components/Layout/Layout';
+
+export default function Home() {
+  return <Layout>jeril</Layout>;
+}
